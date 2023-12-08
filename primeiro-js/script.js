22416167399bf0b65277281650d6764bbf3b9af9
Antolos <acdnf@yahoo.com.br>
2023-12-08 15:26:40 -0300

    primeiro java

---
+++ primeiro-js/script.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+let num3 = 15;
+let num4 = 27;
+console.log(num3+num4);
+
+const imprimir = n => {
+    for(let i = 0; i<n ; i++)
+    {console.log("Conexão feita com sucesso! %d",i + 1)};
+    
+}
+
+imprimir(3);
