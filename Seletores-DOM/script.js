b45741530808dee5588b11a0854afb4a1fa96b00
Antolos <acdnf@yahoo.com.br>
2023-12-08 23:21:08 -0300

    seletorCSSnoDOM

---
+++ Seletores-DOM/script.js
@@ -0,0 +1,10 @@
+const titulo = document.getElementById("titulo");
+const textos = document.getElementsByClassName("texto-simples");
+const selCSS = document.querySelector("div h2");
+
+const textosPorClasse = document.querySelectorAll(".texto-simples");
+
+console.log(titulo);
+console.log(textos);
+console.log(selCSS);
+console.log(textosPorClasse);
