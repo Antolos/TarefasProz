83adc542e6afd1327300c7a27cce24fb3aced3c9
Antolos <acdnf@yahoo.com.br>
2023-12-16 10:45:46 -0300

    for range

---
+++ algoritmo13nao.py
@@ -0,0 +1,16 @@
+i=1
+for i in range(21):
+    if(i != 13):
+        print(f'i {i}')
+
+
+j=1
+while (j <= 20):
+    if(j != 13):
+        print(f'j {j} ')
+    j = j + 1
+
+
+for i in range(20, 0, -1):
+    if(i != 13):
+        print(f'i {i}')
