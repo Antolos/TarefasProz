6601018e70c4e5dbd7fc3f6be9780f0fd62d14b6
Antonio Carlos de Noronha Figueiredo <44371005+Antolos@users.noreply.github.com>
2023-12-18 16:42:57 -0300

    Create Loop.py

---
+++ Loop.py
@@ -0,0 +1,19 @@
+def calculadora(num1, num2, operador):
+   while(operador != sair):
+      if(operador=="soma"):
+        res = num1 + num2
+        print(f'{num1} + {num2} = {res}')
+      elif(operador=="subtrair"):
+        res = num1 - num2
+        print(f'{num1} - {num2} = {res}')
+      elif(operador=="multiplicar"):
+        res = num1 * num2
+        print(f'{num1} * {num2} = {res}')
+      elif(operador=="dividir"):
+        res = num1 / num2
+      print(f'{num1} / {num2} = {res}')
+      num1 = float(input("digite um numero: "))
+      num2 = float(input("digite mais um numero: "))
+      operador = input ("digite soma, subtrair, multiplicar, dividir ou sair para encerrar.")
+
+  print(calculadora(float(input("Digite um número: ")),float(input(Digite outro número: )), input ("digite soma, subtrair, multiplicar, dividir ou sair para encerrar.")))
