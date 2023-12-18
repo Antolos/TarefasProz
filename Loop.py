eed7c4b4ab92b1a1f1bdf25d1632937ff276fd56
Antonio Carlos de Noronha Figueiredo <44371005+Antolos@users.noreply.github.com>
2023-12-18 18:04:15 -0300

    Update Loop.py

--- Loop.py
+++ Loop.py
@@ -1,5 +1,5 @@
 def calculadora(num1, num2, operador):
-   while(operador != sair):
+   while(operador != "sair"):
       if(operador=="soma"):
         res = num1 + num2
         print(f'{num1} + {num2} = {res}')
@@ -16,4 +16,4 @@ def calculadora(num1, num2, operador):
       num2 = float(input("digite mais um numero: "))
       operador = input ("digite soma, subtrair, multiplicar, dividir ou sair para encerrar.")
 
-  print(calculadora(float(input("Digite um número: ")),float(input(Digite outro número: )), input ("digite soma, subtrair, multiplicar, dividir ou sair para encerrar.")))
+print(calculadora(float(input("Digite um número: ")),float(input("Digite outro número: ")), input ("digite soma, subtrair, multiplicar, dividir ou sair para encerrar.")))
