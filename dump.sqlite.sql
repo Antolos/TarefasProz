fcfe229d1a27713da64abc76d9ff679906ac67ec
Antolos <acdnf@yahoo.com.br>
2023-12-29 16:13:25 -0300

    sql esquecido

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -12,13 +12,13 @@ CREATE TABLE Aluno (
   FOREIGN KEY (disciplina_id) REFERENCES Disciplina(disciplina_id) ON UPDATE SET NULL  ON DELETE SET NULL
   );
 CREATE TABLE Disciplina ( 
- disciplina_id int not NULL,
+  disciplina_id int not NULL,
   nome_disciplina varchar(30) NOT NULL DEFAULT ''
 );
  
 --inserts
 INSERT into Disciplina (disciplina_id, nome_disciplina) VALUES (1 , 'Português'),( 2, 'Matemática'),( 3 , ' Computação');
-INSERT into Aluno (id, nome, disciplina_id) VALUES (1 , 'Antonio', 2),( 2, 'João', 3),( 3 , 'Camila', 1), (4, 'Teodoro'); 
+INSERT into Aluno (id, nome, disciplina_id) VALUES (1 , 'Antonio', 2),( 2, 'João', 3),( 3 , 'Camila', 1), (4, 'Teodoro', NULL); 
  
 --inner join
 SELECT nome, nome_disciplina
@@ -28,10 +28,28 @@ INNER JOIN Disciplina ON aluno.disciplina_id = Disciplina.disciplina_id;
 
 -- TRIGGER
 CREATE TRIGGER auto_disciplina
-AFTER INSERT ON Aluno
-FOR EACH ROW
-BEGIN
-  UPDATE Aluno
-  SET disciplina_id =  2
-  WHERE disciplina_id ISNULL;
-END;
+  AFTER INSERT ON Aluno
+  FOR EACH ROW
+  BEGIN
+    UPDATE Aluno
+    SET disciplina_id =  2
+    WHERE disciplina_id ISNULL;
+  END;
+
+-- Função
+CREATE FUNCTION teste (a DECIMAL(4,2), b INT)
+
+ RETURNS int
+
+ DETERMINISTIC
+
+ return a * b;
+
+ -- procedure
+ CREATE PROCEDURE saberPreço (alimento smallint)
+
+select concat('O valor é ', preço) AS preços
+
+from tabela_alimento
+
+where id_alimento = alimento;
